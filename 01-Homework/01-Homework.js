e585d86aff2f83ae90c34c71d458712c6da28604
Sasha <ponomarek91@gmail.com>
2020-01-28 21:11:13 +0200

    My homework added

---
+++ 01-Homework/01-Homework.js
@@ -0,0 +1,8 @@
+let priceOfChocolate = 15.678;
+console.log("I fdvwegvwoivi");
+let priceOfJuice = 123.965;
+let priceOfMilk = 90.2345;
+
+let maximalPrice;
+maximalPrice = Math.max(priceOfChocolate,priceOfJuice,priceOfMilk);
+console.log(maximalPrice);
